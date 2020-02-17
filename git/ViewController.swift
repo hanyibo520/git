508105985548efb0cd924b3ebb8d4ce5346f450f
hanyibo <hanyibo@btte.net>
2020-02-17 14:10:01 +0800

    创建了引导页

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("画做了引导页功能")
     }
 
 
---
+++ git/ViewController2.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController2.swift
+//  git
+//
+//  Created by 韩一博 on 2020/2/17.
+//  Copyright © 2020 韩一博. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+}
