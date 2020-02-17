62b179cadd476ad1091132772746dfdf5591a9dd
hanyibo <hanyibo@btte.net>
2020-02-17 13:41:33 +0800

    Initial Commit

---
+++ gitTests/gitTests.swift
@@ -0,0 +1,34 @@
+//
+//  gitTests.swift
+//  gitTests
+//
+//  Created by 韩一博 on 2020/2/17.
+//  Copyright © 2020 韩一博. All rights reserved.
+//
+
+import XCTest
+@testable import git
+
+class gitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
